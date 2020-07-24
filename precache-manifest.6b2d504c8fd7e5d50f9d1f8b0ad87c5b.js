ca3a2846613aff685846b63a936c6dc4ca667dda
Albatrooss <robillardtim@gmail.com>
2020-07-24 14:21:00 -0400

    Updates

---
+++ precache-manifest.6b2d504c8fd7e5d50f9d1f8b0ad87c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a966a0c0d2f9a85b5936ad38c3bc3f",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "31652c22d098121165b2",
+    "url": "/project-4-assessment/static/css/main.aee65601.chunk.css"
+  },
+  {
+    "revision": "e6f6cd8086db95404770",
+    "url": "/project-4-assessment/static/js/2.d23fdd0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.d23fdd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31652c22d098121165b2",
+    "url": "/project-4-assessment/static/js/main.05cee7b7.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
